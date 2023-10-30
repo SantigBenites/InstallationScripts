92c7e2d17cf0400dcf8521a93e6e01151f555aa3
SantigBenites <santi.mbenites@gmail.com>
2023-10-30 11:32:20 +0000

    fixed problems with mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -1,12 +1,10 @@
 sudo apt-get install gnupg curl
 
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc 
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
 
-sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gp --dearmor
-
-echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bullseye/mongodb-org/7.0 main"
-
-sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bullseye/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
 sudo apt-get update
 
