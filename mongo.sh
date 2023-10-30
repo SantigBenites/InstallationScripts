4736a3ec271ddf2f2cd101fb0875c227ae7ef252
SantigBenites <santi.mbenites@gmail.com>
2023-10-30 11:25:05 +0000

    added mongo installation script

---
+++ mongo.sh
@@ -0,0 +1,20 @@
+sudo apt-get install gnupg curl
+
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc 
+
+sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gp --dearmor
+
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bullseye/mongodb-org/7.0 main"
+
+sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-database hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-mongosh hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
