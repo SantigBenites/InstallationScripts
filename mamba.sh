3a37b492389e772310014f857f92110bba9e240d
Santiago Benites <63800120+SantigBenites@users.noreply.github.com>
2023-11-20 11:12:31 +0000

    Added comma to end of curl

--- mamba.sh
+++ mamba.sh
@@ -1,3 +1,3 @@
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh"
 
 bash Mambaforge-Linux-x86_64.sh
